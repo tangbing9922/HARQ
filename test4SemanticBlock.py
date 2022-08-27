fe4486a12fe011561764d71d28b5119493e34bbe
bing@108lab <1260069314@qq.com>
2022-08-27 13:49:21 +0800

    0827 格式修改

--- test4SemanticBlock.py
+++ test4SemanticBlock.py
@@ -22,7 +22,7 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('--vocab_file', default='./europarl/vocab32.json', type=str)
 parser.add_argument('--checkpoint_path', default='./checkpoints', type=str)
-parser.add_argument('--channel', default='AWGN_Relay', type=str, help = 'Please choose AWGN, Rayleigh, and Rician')
+parser.add_argument('--channel', default='AWGN_Relay', type=str, help='Please choose AWGN, Rayleigh, and Rician')
 parser.add_argument('--MAX_LENGTH', default=32, type=int)
 parser.add_argument('--MIN_LENGTH', default=4, type=int)
 parser.add_argument('--d_model', default=128, type=int)
@@ -30,7 +30,6 @@ parser.add_argument('--dff', default=512, type=int)
 ###########################################################
 parser.add_argument('--num_layers', default=3, type=int)
 parser.add_argument('--num_heads', default=8, type=int)
-
 
 if __name__ == '__main__':
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -41,19 +40,19 @@ if __name__ == '__main__':
     num_vocab = len(token_to_idx)
     pad_idx = token_to_idx["<PAD>"]
     start_idx = token_to_idx["<START>"]
-    end_idx = token_to_idx["<END>"]#2
+    end_idx = token_to_idx["<END>"]  # 2
     model = DeepTest(args.num_layers, num_vocab, num_vocab,
-                        args.MAX_LENGTH, args.MAX_LENGTH, args.d_model, args.num_heads,
-                        args.dff, 0.1).to(device)
-    SemanticBlock_checkpoint = torch.load('./checkpoints/Train_Destination_SemanticBlock_withoutQ/0727DeepTest_net_checkpoint.pth')# Desti
-    SemanticBlock_checkpoint = torch.load('./checkpoints/Train_SemanticBlock/0727DeepTest_net_checkpoint.pth')# Relay
+                     args.MAX_LENGTH, args.MAX_LENGTH, args.d_model, args.num_heads,
+                     args.dff, 0.1).to(device)
+    SemanticBlock_checkpoint = torch.load('./checkpoints/Train_Destination_SemanticBlock_withoutQ/0727DeepTest_net_checkpoint.pth')  # Desti
+    SemanticBlock_checkpoint = torch.load('./checkpoints/Train_SemanticBlock/0727DeepTest_net_checkpoint.pth')  # Relay
     model.load_state_dict(SemanticBlock_checkpoint['model'])
     mi_net = Mine().to(device)
     mi_checkpoint = torch.load('./checkpoints/Train_SemanticBlock/0727mi_net_checkpoint.pth')
     mi_net.load_state_dict(mi_checkpoint['model'])
 
     sentences = ['the events taking place today in school are extremely interesting']
-    StoT = SeqtoText(token_to_idx,start_idx, end_idx)
+    StoT = SeqtoText(token_to_idx, start_idx, end_idx)
     model.eval()
     mi_net.eval()
 
